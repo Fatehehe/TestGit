6485c2e5adf93e3f39cdc0a332caa2749be7b4dc
Fatih <fatakhillah.khaqo@gmail.com>
2024-05-06 17:02:21 +0700

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Fatakhillah Khaqo on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
